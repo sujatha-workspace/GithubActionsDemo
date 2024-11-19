4e32134d6faf95575828147045a394e56a5e6c0e
sujatha-workspace <suji1229@gmail.com>
2024-11-19 19:26:23 +0530

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Development environment variables
+DB_HOST="dev-db.example.com"
+DB_USER="dev_user"
+DB_PASS="dev_password"
+
+# Use the variables in your script
+echo "Connecting to the development database..."
+echo "Host: $DB_HOST"
+echo "User: $DB_USER"
